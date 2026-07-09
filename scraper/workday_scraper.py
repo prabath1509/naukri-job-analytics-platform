caa4fe0dd92083d6d97dfde8d2e90d0e27e5daa6
Krishna Prabath <krishnaprabath1509@gmail.com>
2026-07-09 15:32:11 +0530

    Expand verified Workday ATS sources

--- scraper/workday_scraper.py
+++ scraper/workday_scraper.py
@@ -6,16 +6,28 @@ import requests
 
 WORKDAY_COMPANIES = [
     {
-        "company": "NVIDIA",
-        "url": "https://nvidia.wd5.myworkdayjobs.com/wday/cxs/nvidia/ExternalCareerSite/jobs",
-    },
-    {
         "company": "Mastercard",
         "url": "https://mastercard.wd1.myworkdayjobs.com/wday/cxs/mastercard/CorporateCareers/jobs",
     },
     {
-        "company": "GE Aerospace",
-        "url": "https://geaerospace.wd5.myworkdayjobs.com/wday/cxs/geaerospace/External/jobs",
+        "company": "Salesforce",
+        "url": "https://salesforce.wd12.myworkdayjobs.com/wday/cxs/salesforce/External_Career_Site/jobs",
+    },
+    {
+        "company": "Adobe",
+        "url": "https://adobe.wd5.myworkdayjobs.com/wday/cxs/adobe/external_experienced/jobs",
+    },
+    {
+        "company": "Intel",
+        "url": "https://intel.wd1.myworkdayjobs.com/wday/cxs/intel/External/jobs",
+    },
+    {
+        "company": "PayPal",
+        "url": "https://paypal.wd1.myworkdayjobs.com/wday/cxs/paypal/jobs/jobs",
+    },
+    {
+        "company": "Autodesk",
+        "url": "https://autodesk.wd1.myworkdayjobs.com/wday/cxs/autodesk/Ext/jobs",
     },
 ]
 
